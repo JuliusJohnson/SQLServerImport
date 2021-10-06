6e1c3bf934b569ba9db10a206c92c26a5d1caffe
Julius <contact@juliusj.com>
2021-10-05 21:37:56 -0400

    basic error handing of sql insert + added pprint

--- sa.py
+++ sa.py
@@ -1,7 +1,9 @@
 #importing sql library
 import sqlalchemy, os
 import pandas as pd
+from sqlalchemy.exc import ProgrammingError
 from dev import settings as setting
+from pprint import pprint
 
 
 from sqlalchemy.sql.sqltypes import VARCHAR
@@ -32,7 +34,6 @@ def csvToSQL(isCustom,filePath):
 
         else:
             dataTypeFile = pd.read_csv(filePath)
-            print(len(dataTypeFile.columns))
             if dataTypeFile.shape[0] == numOfClolumnsInData:
                 while i < numOfClolumnsInData:
                     for column in df.columns:
@@ -41,21 +42,26 @@ def csvToSQL(isCustom,filePath):
                         elif dataTypeFile["Datatype"][i] == "Int":
                             dataTypeDict[column] = sqlalchemy.types.INT
                         else:
-                            print("Invalid")
+                            pprint("Invalid")
                         i = i + 1
             else:
-                print("Please check the number of columns in the specified spreadsheet. They do not match the number of columns in the CSV you want to import to SQL Server")
+                pprint("Please check the number of columns in the specified spreadsheet. They do not match the number of columns in the CSV you want to import to SQL Server")
                 break
                 
-        print(dataTypeDict)
+        #print(dataTypeDict)
         # Create Table
         tableName = str(str(file).replace(".csv",""))
     
-        #Insert Dataframe into SQL Server. If the Table exisit alreadt it will replace. 
-        df.to_sql(tableName, con = engine, if_exists='replace', index = False, dtype=dataTypeDict, schema=schema)
+        #Insert Dataframe into SQL Server. If the Table exisit alreadt it will replace.
+        try:
+            df.to_sql(tableName, con = engine, if_exists='replace', index = False, dtype=dataTypeDict, schema=schema)
+        except ProgrammingError:
+            pprint("There is likely a problem with the settings entered. Please double check server permissions and specified database settings.")
+            break
     
         # show the complete data
         # from Employee_Data table
-        print(engine.execute(f"SELECT TOP (5) * FROM {tableName}").fetchall())
+        pprint(engine.execute(f"SELECT TOP (5) * FROM {tableName}").fetchall())
+        pprint("Job Complete")
 
 csvToSQL(True,"datatypeSheet.csv")
